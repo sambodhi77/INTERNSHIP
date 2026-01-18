b4a00d4518e358600aa8e67418890729e7ad0a6a
sambodhi katarnavare <sambodhikatarnwre@gmail.com>
2026-01-18 12:03:39 +0530

    javascript

---
+++ day1.js
@@ -0,0 +1,26 @@
+let fruits = ["apple", "banana", "mango", "orange"];
+
+let Students = 
+{
+    Name : "Amit",
+    age : 20,
+    course : "CS"
+}
+
+console.log(fruits[0]);
+
+console.log(Students.Name);
+
+console.log(Students.age);
+
+// console.log (Students.course);
+
+console.log(Students["course"])
+
+Students.city = "Nashik"
+
+console.log(Students["city"]);
+
+let Students = [
+    
+]
